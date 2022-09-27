71c65d60b0de48376cd9d2a5ec84e7a751d194ac
mgntpk <mgntpkgit@gmail.com>
2022-09-27 13:34:05 +0900

    日付等の0埋めの方法を変更。 sliceからpadStartに変更。

--- js/main.js
+++ js/main.js
@@ -4,13 +4,14 @@ function DownloadTxt(str) {
     link.href = URL.createObjectURL(blob);
     const d = new Date();
     const year = d.getFullYear(),
-        month = ( '00' + (d.getMonth() + 1)).slice(-2),
-        date = ( '00' + d.getDate()).slice(-2),
-        hours = ( '00' + d.getHours()).slice(-2),
-        minutes = ( '00' + d.getMinutes()).slice(-2),
-        seconds = ( '00' + d.getSeconds()).slice(-2),
-        milliseconds = ( '000' + d.getMilliseconds()).slice(-3);
+        month = String((d.getMonth() + 1)).padStart(2,"0"),
+        date = String(d.getDate()).padStart(2,"0"),
+        hours = String(d.getHours()).padStart(2,"0"),
+        minutes = String(d.getMinutes()).padStart(2,"0"),
+        seconds = String(d.getSeconds()).padStart(2,"0"),
+        milliseconds = String(d.getMilliseconds()).padStart(3,"0");
     link.download = `memo${year}${month}${date}${hours}${minutes}${seconds}${milliseconds}.txt`;
+    console.log(`${year}/${month}/${date}/${hours}/${minutes}/${seconds}/${milliseconds}`);
     link.click();
     URL.revokeObjectURL(link.href);
 }
